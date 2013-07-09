b5927eef11f0132ef0c7f59045b55b0caa33119b
Jose Perdomo <jepz20@gmail.com>
2013-07-09 22:37:27 +0000

    Se corrigio web.js

--- web.js
+++ web.js
@@ -2,13 +2,11 @@ var express = require('express');
 var fs = require ('fs'); 
 var app = express.createServer(express.logger());
 
-var index = fs.readFile('index.html', function (err, data) {
+app.get('/', function(request, response) {
+fs.readFile('index.html', function (err, data) {
   if (err) throw err;
   console.log(data.toString('utf8',0));
-}); 
-
-app.get('/', function(request, response) {
-  response.send(index.toString('utf8',0));
+(index.toString('utf8',0));
 });
 
 var port = process.env.PORT || 5000;
